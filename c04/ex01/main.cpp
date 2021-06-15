f7ac32898693b266e978e638ba38e711f7b7bab5
souadmaniani <soadmaniani98@gmail.com>
2021-06-15 21:27:28 +0100

    c02

---
+++ c04/ex01/main.cpp
@@ -0,0 +1,21 @@
+
+// int main()
+// {
+//     Character* me = new Character("me");
+//     std::cout << *me;
+//     Enemy* b = new RadScorpion();
+//     AWeapon* pr = new PlasmaRifle();
+//     AWeapon* pf = new PowerFist();
+//     me->equip(pr);
+//     std::cout << *me;
+//     me->equip(pf);
+//     me->attack(b);
+//     std::cout << *me;
+//     me->equip(pr);
+//     std::cout << *me;
+//     me->attack(b);
+//     std::cout << *me;
+//     me->attack(b);
+//     std::cout << *me;
+//     return 0;
+// }
