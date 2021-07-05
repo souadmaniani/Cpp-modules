66fa936f612dce01ab307e211a67ef1eca8d9991
souadmaniani <soadmaniani98@gmail.com>
2021-07-05 23:37:37 +0000

    still assignation operator

--- c08/ex02/main.cpp
+++ c08/ex02/main.cpp
@@ -1,13 +1,15 @@
 #include "mutantstack.hpp"
 #include <vector>
+
 int main()
 {
-    std::vector<float> v;
-    v.push_back(44444);
-    std::stack<int> m_stack(std::deque<int>(v.begin(), v.end()));
-    std::cout << m_stack.top() << "\n";
+    // std::vector<float> v;
+    // v.push_back(44444);
+    // std::stack<int> m_stack(std::deque<int>(v.begin(), v.end()));
+    // std::cout << m_stack.top() << "\n";
 
     // std::vector<int> m_vec;
+
     // std::stack<int, std::vector<int> > m_stack(m_vec);
 
     MutantStack<int>    mstack;
@@ -20,23 +22,25 @@ int main()
     mstack.pop();
 
     std::cout << mstack.size() << std::endl;
-
+    mstack.push(42);
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
     mstack.push(0);
+    mstack.push(1337);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
 
-    // MutantStack<int>::iterator it = mstack.begin();
-    // MutantStack<int>::iterator ite = mstack.end();
-
-    // ++it;
-    // --it;
-    // while(it != ite)
-    // {
-    //     std::cout << *it << std::endl;
-    //     ++it;
-    // }
-    // std::stack<int> s(mstack);
+    ++it;
+    --it;
+    while(it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+    std::cout << s.top() << "\n";
+    MutantStack<int> test = mstack;
+    std::cout << test.top() << "\n";
     return 0;
 }
--- c08/ex02/mutantstack.hpp
+++ c08/ex02/mutantstack.hpp
@@ -4,35 +4,69 @@
 #include <iostream>
 
 // template <class Type, class Container = deque<Type> > class stack;
+// typename here is letting the compiler know that value_type is a type
+// and not a static member of _MyBase
 // typedef typename _Mybase::value_type value_type;
+// ========> typedef typename std::vector<T>::iterator Iter
+
+// The standard container classes vector, deque and list fulfill
+// these requirements. By default, if no container class is specified
+// for a particular stack class instantiation, the standard container deque is used
+
+// std::vector<int> v{1, 2, 3, 4, 5};
+// for (std::vector<int>::reverse_iterator it = v.rbegin(); it != v.rend(); ++it)
+// {
+//     cout << *it;
+// } // prints 54321
 
 template <typename T>
 class MutantStack : public std::stack<T>
 {
 public:
-    MutantStack();
-    ~MutantStack();
-    MutantStack(MutantStack const & src);
-    MutantStack & operator=(MutantStack const & rhs);
+    MutantStack(){}
+    ~MutantStack(){
+        std::cout << "MutantStack Destructor\n";
+    }
+    MutantStack(MutantStack const & src){
+        std::cout << "MutantStack copy\n";
+        *this = src;
+    }
+
+    MutantStack & operator=(MutantStack const & rhs){
+        std::cout << "MutantStack assignation operator\n";
+        
+        return (*this);
+    }
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+    typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+    iterator begin(){
+        return this->c.begin();
+    }
+    const_iterator begin()const{
+        return  this->c.begin();
+    }
+    iterator end(){
+        return  this->c.end();
+    }
+    const_iterator end() const{
+        return  this->c.end();
+    }
+    reverse_iterator rbegin(){
+        return  this->c.rbegin();
+    }
+    const_reverse_iterator rbegin() const{
+        return  this->c.rbegin();
+    }
+    reverse_iterator rend(){
+        return  this->c.rend();
+    }
+    const_reverse_iterator rend() const{
+        return  this->c.rend();
+    }
 
 };
-
-template <typename T>
-MutantStack<T>::MutantStack()
-{}
-template <typename T>
-MutantStack<T>::~MutantStack()
-{}
-template <typename T>
-MutantStack<T>::MutantStack(MutantStack const & src)
-{
-    *this = src;
-}
-template <typename T>
-MutantStack<T> & MutantStack<T>::operator=(MutantStack const & rhs)
-{
-    (void)rhs;
-    return (*this);
-}
-
+ 
 #endif
